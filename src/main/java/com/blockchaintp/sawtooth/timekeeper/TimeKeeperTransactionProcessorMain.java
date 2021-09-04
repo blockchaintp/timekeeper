69def1e748896865d5ec68bd589b6c50fceba1e4
Kevin O'Donnell <kevin@blockchaintp.com>
2021-09-04 10:24:25 -0400

    feat: allow to start either submitter, tp, or both
    Signed-off-by: Kevin O'Donnell <kevin@blockchaintp.com>

--- src/main/java/com/blockchaintp/sawtooth/timekeeper/TimeKeeperTransactionProcessorMain.java
+++ src/main/java/com/blockchaintp/sawtooth/timekeeper/TimeKeeperTransactionProcessorMain.java
@@ -11,18 +11,24 @@
 ------------------------------------------------------------------------------*/
 package com.blockchaintp.sawtooth.timekeeper;
 
+import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.blockchaintp.sawtooth.messaging.ZmqStream;
-import com.blockchaintp.sawtooth.timekeeper.processor.TimeKeeperTransactionHandler;
 import com.blockchaintp.keymanager.InMemoryKeyManager;
 import com.blockchaintp.keymanager.KeyManager;
+import com.blockchaintp.sawtooth.messaging.ZmqStream;
+import com.blockchaintp.sawtooth.timekeeper.processor.TimeKeeperTransactionHandler;
 import com.blockchaintp.utils.LogUtils;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionGroup;
+import org.apache.commons.cli.Options;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import sawtooth.sdk.messaging.Stream;
 import sawtooth.sdk.processor.TransactionHandler;
@@ -30,12 +36,26 @@ import sawtooth.sdk.processor.TransactionProcessor;
 
 /**
  * A basic Main class for TimeKeeperTransactionProcessor.
- * @author scealiontach
  */
 public final class TimeKeeperTransactionProcessorMain {
 
   private static final int DEFAULT_TK_UPDATE_SECONDS = 20;
+  private static final String DEFAULT_CONNECT_STRING = "tcp://localhost:4004";
+
   private static final Logger LOGGER = LoggerFactory.getLogger(TimeKeeperTransactionProcessorMain.class);
+
+  private static final String OPT_CONNECT = "C";
+  private static final String OPT_PERIOD = "p";
+  private static final String OPT_SUBMITTER = "s";
+  private static final String OPT_TP = "t";
+  private static final String OPT_BOTH = "b";
+  private static final String OPT_VERBOSE = "v";
+
+  int vCount = 0;
+  int updatePeriod = DEFAULT_TK_UPDATE_SECONDS;
+  String connectStr = DEFAULT_CONNECT_STRING;
+  boolean startTp = true;
+  boolean startSubmitter = true;
 
   /**
    * A basic main method for this transaction processor.
@@ -43,52 +63,43 @@ public final class TimeKeeperTransactionProcessorMain {
    *             component endpoint, e.g. tcp://localhost:4004
    */
   public static void main(final String[] args) {
-
-    var vCount = 0;
-    var connectStr = "tcp://localhost:4004";
-    long period = DEFAULT_TK_UPDATE_SECONDS;
-
-    for (String s : args) {
-      if (s.startsWith("-v")) {
-        for (var i = 0; i < s.length(); i++) {
-          if (s.charAt(i) == 'v') {
-            vCount++;
-          }
-        }
-      } else if (s.startsWith("-p")) {
-        var val = s.substring(2);
-        if (val.length() > 0) {
-          try {
-            period = Integer.valueOf(val);
-          } catch (NumberFormatException nfe) {
-            LOGGER.warn("Invalid format specified for period: {}", val);
-            period = DEFAULT_TK_UPDATE_SECONDS;
-          }
-        }
-      } else {
-        connectStr = s;
-      }
+    final TimeKeeperTransactionProcessorMain main = new TimeKeeperTransactionProcessorMain();
+    try {
+      main.parseArgs(args);
+    } catch (InvalidCommandException e) {
+      System.err.println(e.getMessage()); //NOSONAR
+      System.exit(-1);
     }
+
+  }
+
+  public void start() {
     LogUtils.setRootLogLevel(vCount);
 
     ScheduledExecutorService clockExecutor = Executors.newSingleThreadScheduledExecutor();
 
-    Stream stream = new ZmqStream(connectStr);
-    KeyManager kmgr = InMemoryKeyManager.create();
-    clockExecutor.scheduleWithFixedDelay(new TimeKeeperRunnable(kmgr, stream), period, period, TimeUnit.SECONDS);
+    Stream stream = new ZmqStream(connectStr); //NOSONAR
 
-    TransactionProcessor transactionProcessor = new TransactionProcessor(connectStr);
-    TransactionHandler handler = new TimeKeeperTransactionHandler();
-    transactionProcessor.addHandler(handler);
+    if (startSubmitter) {
+      KeyManager keyManager = InMemoryKeyManager.create();
+      clockExecutor.scheduleWithFixedDelay(new TimeKeeperRunnable(keyManager, stream), updatePeriod, updatePeriod,
+          TimeUnit.SECONDS);
+    }
 
-    Thread thread = new Thread(transactionProcessor);
-    thread.start();
-    try {
-      thread.join();
-      clockExecutor.shutdownNow();
-    } catch (InterruptedException exc) {
-      LOGGER.warn("TransactionProcessor was interrupted");
-      Thread.currentThread().interrupt();
+    if (startTp) {
+      TransactionProcessor transactionProcessor = new TransactionProcessor(connectStr);
+      TransactionHandler handler = new TimeKeeperTransactionHandler();
+      transactionProcessor.addHandler(handler);
+
+      Thread thread = new Thread(transactionProcessor);
+      thread.start();
+      try {
+        thread.join();
+        clockExecutor.shutdownNow();
+      } catch (InterruptedException exc) {
+        LOGGER.warn("TransactionProcessor was interrupted");
+        Thread.currentThread().interrupt();
+      }
     }
   }
 
@@ -96,4 +107,107 @@ public final class TimeKeeperTransactionProcessorMain {
     // private constructor for utility class
   }
 
+  private Options createOptions() {
+    Option connect = Option.builder("C")
+                            .argName("endpoint")
+                            .longOpt("connect")
+                            .hasArg()
+                            .required(false)
+                            .desc("Give the validator ZMQ endpoint to connect to")
+                            .build();
+
+    Option period = Option.builder("p")
+                          .argName("period")
+                          .longOpt("period")
+                          .hasArg()
+                          .desc("Ho often to send time updates")
+                          .build();
+
+    Option verbose = Option.builder("v")
+                          .desc("Verbosity. Repeat for greater detail.")
+                          .build();
+
+
+    Option submitter = Option.builder("s")
+                          .longOpt("submitter")
+                          .desc("Set to run the submitter")
+                          .build();
+
+    Option tp = Option.builder("t")
+                      .longOpt("tp")
+                      .desc("Set to run the transaction processr")
+                      .build();
+
+    Option both = Option.builder("b")
+                        .longOpt("both")
+                        .desc("Set to run both the tp and the submitter [default]")
+                        .build();
+
+    OptionGroup mode = new OptionGroup();
+    mode.setRequired(false);
+    mode.addOption(submitter);
+    mode.addOption(both);
+    mode.addOption(tp);
+
+    Options options = new Options();
+    options.addOption(connect);
+    options.addOption(period);
+    options.addOption(verbose);
+    options.addOptionGroup(mode);
+
+    return options;
+  }
+
+  private void parseArgs(String[] args) throws InvalidCommandException {
+    Options options = createOptions();
+
+    CommandLineParser parser = new org.apache.commons.cli.DefaultParser();
+    CommandLine cmd = null;
+    try {
+      cmd = parser.parse(options, args);
+    } catch (org.apache.commons.cli.ParseException e) {
+      throw new InvalidCommandException(String.format("Invalid command line arguments: %s", e.getMessage()));
+    }
+
+    if (cmd.hasOption(OPT_CONNECT)) {
+      connectStr = cmd.getOptionValue(OPT_CONNECT);
+    }
+
+    if (cmd.hasOption(OPT_PERIOD)) {
+      var periodStr = cmd.getOptionValue(OPT_PERIOD);
+      try {
+        updatePeriod = Integer.parseInt(periodStr);
+      } catch (NumberFormatException nfe) {
+        throw new InvalidCommandException(String.format("Invalid format specified for period: %s", periodStr));
+      }
+    }
+
+    if (cmd.hasOption(OPT_VERBOSE)) {
+      for (Option o: cmd.getOptions()) {
+        if (o.getOpt().equals(OPT_VERBOSE)) {
+          vCount++;
+        }
+      }
+    }
+
+    startTp = true;
+    startSubmitter = true;
+
+    if (cmd.hasOption(OPT_SUBMITTER)) {
+      startSubmitter = true; //NOSONAR
+      startTp = false;
+    } else if (cmd.hasOption(OPT_TP)) {
+      startSubmitter = false;
+      startTp = true; //NOSONAR
+    } else if (cmd.hasOption(OPT_BOTH)) {
+      startSubmitter = true; //NOSONAR
+      startTp = true; //NOSONAR
+    }
+
+    List<String> remainder = cmd.getArgList();
+
+    if (!remainder.isEmpty()) {
+      connectStr = remainder.get(0);
+    }
+  }
 }
